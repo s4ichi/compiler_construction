e26923cc920aa9c417bd4568cf641891d26f26a5
everysick <s.wakeup31@gmail.com>
2015-10-02 11:56:23 +0900

    initial commit

---
+++ build.sbt
@@ -0,0 +1,11 @@
+lazy val root = (project in file(".")).
+  settings(
+    name := "compiler_construction",
+    version := "1.0"
+  )
+
+/*
+ * java version "1.8.0_45"
+ * Java(TM) SE Runtime Environment (build 1.8.0_45-b14)
+ * Java HotSpot(TM) 64-Bit Server VM (build 25.45-b02, mixed mode)
+*/
---
+++ src/main/java/rec1/FileLexerRunner.java
@@ -0,0 +1,15 @@
+package chap3;
+import stone.*;
+import java.io.FileNotFoundException;
+
+public class FileLexerRunner {
+    public static void main(String[] args) throws ParseException {
+        try {
+            Lexer l = new Lexer(CodeDialog.file());
+            for (Token t; (t = l.read()) != Token.EOF; )
+                System.out.println("=> " + t.getText());
+        } catch(FileNotFoundException f) {
+            System.out.println(f);
+        }
+    }
+}
